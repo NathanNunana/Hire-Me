16bf5674a3dc6bf9dca5a954e177eef68cd5c09f
Nathan Nunana Kulewoshie <55038849+NathanNunana@users.noreply.github.com>
2023-03-03 12:52:48 +0000

    network util

--- lib/utils/network.dart
+++ lib/utils/network.dart
@@ -3,14 +3,15 @@ part of hireme_utils;
 class NetworkUtil {
   // bearer token
   static String? _getBearerToken() {
-    // TODO: implement get bearer token
-    return '';
+    return HiveServiceImpl().retrieveToken();
   }
+
+  final String baseDomain = HiremeConfig.instance!.values.baseDomain;
 
   // Get Request
   Future getReq(String url) async {
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await http.get(Uri.parse('$baseDomain/$url'));
       if (response.statusCode == 200) {
         return response.body;
       } else {
