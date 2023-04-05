0663b4deac2ed8b201ce941945be7f357cb1b5d1
Abel DropDout <abeldzan@gmail.com>
2023-04-05 19:51:07 +0000

    feat: :sparkles: added supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hire_me/services/_index.dart';
 import 'package:hire_me/utils/_index.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   HiremeConfig(
     values: HiremeValues(
       baseDomain: 'http://127.0.0.1:8000/api', //replace baseDomain
@@ -13,6 +16,11 @@ void main() async {
   );
 
   await HiveServiceImpl().initBoxes();
+
+  await Supabase.initialize(
+    url: dotenv.get("SUPABASE_URL", fallback: ""),
+    anonKey: dotenv.get("SUPABASE_ANON", fallback: ""),
+  );
 
   return runApp(
     MultiProvider(
